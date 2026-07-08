54629f4733052be02c842925dc495872b5ed6f66
Vishal S <vishalshanmugam928@gmail.com>
2026-07-08 21:04:32 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,19 +8,19 @@ function Projects() {
       title: "Social Media App",
       description: "A full-stack social media project with login and logout page.",
       tech: "Node.js, Express, MongoDB, React",
-      link: "https://social-media-9jg8.onrender.com",
+      link: "media-guvi.netlify.app",
     },
     {
       title: "E-commerce Website",
       description: "A React + Django web app to search Products using an external API.",
       tech: "React, Node.js, API Integration",
-      link: "https://ecommerce-2-frontend-0wev.onrender.com",
+      link: "ecomerce-guvi.netlify.app",
     },
      {
       title: "Youtube Clone",
       description: "A Youtube clone with search functionality and youtube videos.",
       tech: "Node.js, Express, MongoDB,React",
-      link: "https://youtube-frontend-rqba.onrender.com",
+      link: "clone-ytub.netlify.app",
     },
   ];
 
